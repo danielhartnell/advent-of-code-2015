dc1cf43eb2025b6be2fffe451d42f09290c91ef1
Daniel Hartnell <dhartnell@newrelic.com>
2015-12-08 20:16:13 -0800

    include challenge 1, part 2

---
+++ challenge-1-part2.rb
@@ -0,0 +1,17 @@
+#!/usr/bin/env ruby
+# Challenge-1: http://adventofcode.com/
+# By Daniel Hartnell
+
+def is_basement(floor, index)
+  if floor == -1
+    # Since array index starts at zero but we want position I'll increment index by 1
+    puts "#{floor} occurs at index: #{index+1}"
+  end
+end
+
+floor = 0
+directions = File.open("./challenge-1-input.txt", "r").read.chomp.split('').each_with_index do |value, index|
+  floor += 1 if value == "("
+  floor -= 1 if value == ")"
+  is_basement(floor, index)
+end
